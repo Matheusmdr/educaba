7304629f0203033d0df5e0451bcce4a8567a46d0
Matheusmdr <matheus.m.rocha@unesp.br>
2024-10-19 17:29:20 -0300

    feat(mobile): complete tab bar implementation with icons
    - Added icons for the tab bar to enhance navigation
    - Finalized the layout and functionality of the tab bar
    - Ensured smooth transitions and consistent styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,10 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBarIcon } from "@/components/navigation/tabbar-icon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { View } from "react-native";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -11,24 +12,64 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
-      }}>
+        tabBarLabel: () => null,
+        tabBarStyle: {
+          backgroundColor: "#fff",
+          height: 70,
+          borderTopStartRadius: 20,
+          borderTopEndRadius: 20,
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 10 },
+          shadowOpacity: 0.1,
+          elevation: 10,
+          overflow: "hidden",
+          position: "absolute",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon
+              name={focused ? "home-filled" : "home"}
+              focused={focused}
+            />
           ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="programs"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon
+              name={focused ? "programs-outline" : "programs"}
+              focused={focused}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="analytics"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon
+              name={focused ? "analytics-outline" : "analytics"}
+              focused={focused}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="config"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon
+              name={focused ? "settings-outline" : "settings"}
+              focused={focused}
+            />
           ),
         }}
       />
